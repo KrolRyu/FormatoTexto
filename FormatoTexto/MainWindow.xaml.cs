d026a4349165f1d25d67e14fc3488fdc204f3e24
KrolRyu <mario.martos@iesdoctorbalmis.com>
2021-09-29 12:00:22 +0200

    Agregar archivos de proyecto.

---
+++ FormatoTexto/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FormatoTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextoFinalTextBlock.Text = TextoTextBox.Text;
+        }
+
+        private void NegritaCheckBox_Checked(object sender, RoutedEventArgs e)
+        {          
+                TextoFinalTextBlock.FontWeight = FontWeights.Bold;
+        }
+
+    }
+}
