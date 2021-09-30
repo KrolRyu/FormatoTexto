7b575ebe2dada2db0ea846b6047aa1f2e7a65d37
KrolRyu <mario.martos@iesdoctorbalmis.com>
2021-09-30 11:24:09 +0200

    Ejercicio terminado

--- FormatoTexto/MainWindow.xaml.cs
+++ FormatoTexto/MainWindow.xaml.cs
@@ -23,5 +23,33 @@ namespace FormatoTexto
                 TextoFinalTextBlock.FontWeight = FontWeights.Bold;
         }
 
+        private void CursivaCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            TextoFinalTextBlock.FontStyle = FontStyles.Italic;
+        }
+
+        private void NegritaCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            TextoFinalTextBlock.FontWeight = FontWeights.Normal;
+        }
+        private void CursivaCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            TextoFinalTextBlock.FontStyle = FontStyles.Normal;
+        }
+
+        private void AzulRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            TextoFinalTextBlock.Foreground = System.Windows.Media.Brushes.Blue;
+        }
+
+        private void RojoRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            TextoFinalTextBlock.Foreground = System.Windows.Media.Brushes.Red;
+        }
+
+        private void VerdeRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            TextoFinalTextBlock.Foreground = System.Windows.Media.Brushes.Green;
+        }
     }
 }
